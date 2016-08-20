a205f3e35b3c27d70073f5fcc28557dca30718bd
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2016-08-20 16:26:49 +0530

    Custom editors

---
+++ Assets/Editor/GameMangerEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GameManager))]
+public class GameMangerEditor : Editor {
+
+	public override void OnInspectorGUI() {
+
+		DrawDefaultInspector ();
+
+		GameManager myGM = (GameManager)target;
+
+		if (GUILayout.Button ("Reset Player State")) {
+			PlayerPrefManager.ResetPlayerState (myGM.startLives, false);
+		}
+
+		if (GUILayout.Button ("Reset High Score")) {
+			PlayerPrefManager.SetHighscore (0);
+		}
+
+		if (GUILayout.Button("Output Player State")) {
+			PlayerPrefManager.ShowPlayerPrefs();
+		}
+	}
+}
